5e0b6577cc69a5a8b5d611f3c409840927b4e97d
tarikm2 <tarikm2@uw.edu>
2017-02-19 18:15:29 -0800

    added a basic models route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const routes = require ("./routes");
 
-const app = reqire("express")();
+const app = require("express")();
 app.use("/", routes);
 app.listen(3000, () => {
     console.log("App is listening to port 3000");
