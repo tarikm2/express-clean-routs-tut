c3730efb75ea1c9a3d9115f50d0103e71e211982
tarikm2 <tarikm2@uw.edu>
2017-02-20 21:20:49 -0800

    added /models middleware to validate the modelId coming into requests on that route

--- routes/models/index.js
+++ routes/models/index.js
@@ -2,9 +2,23 @@ const models = require("express").Router();
 const all = require('./all.js');
 const single = require('./single.js');
 const cars = require('./cars/index.js');
+const data = require("../../data.json");
 
 models.get('/', all);
 models.get('/:modelId', single);
 models.use('/:modelId/cars', cars);
+
+//this is parameter middleare for the modelId parameter.
+//ensures a model exsists. if it does not return a 404
+models.param('modelId', (req, res, next, value) => {
+    const model = data.models.find(m => m.id === (value * 1));
+
+    if(model) {
+	req['modle'] = model;
+	next();
+    } else {
+	res.status(404).send("Error: invalid model");
+    }
+});
 
 module.exports = models;
